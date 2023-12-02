ac27e94730ab4abd47d81ec9d846412dbf45c41f
Amaizzzzz <112814055+Amaizzzzz@users.noreply.github.com>
2023-12-02 14:42:55 -0800

    a6 Mongo fa23

--- app.js
+++ app.js
@@ -1,31 +1,54 @@
-import express from 'express';
-import Hello from "./hello.js"
+// const express = require("express");
+import session from "express-session";
+import "dotenv/config";
+import express from "express";
+import EnrollmentRoutes from "./enrollments/routes.js";
+import HelloRoutes from "./hello.js";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
-import cors from "cors";
 import ModuleRoutes from "./modules/routes.js";
-import AssignmentRoutes from './assignments/routes.js';
-import "dotenv/config";
+import UserRoutes from "./users/routes.js";
+import mongoose from "mongoose";
+import LikesRoutes from "./likes/routes.js";
+import FollowsRoutes from "./follows/routes.js";
+import SectionRoutes from "./sections/routes.js";
+import cors from "cors";
 
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas-cs5610-fa23");
 
 const app = express();
-const allowedOrigins = ['http://localhost:3000', 'https://a5--aesthetic-smakager-441fa4.netlify.app'];
-
-app.use(cors({
+app.use(
+  cors({
     credentials: true,
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    }
-}));
+    origin: process.env.FRONTEND_URL
+  })
+);
+
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+
 
 app.use(express.json());
-CourseRoutes(app);
+
+FollowsRoutes(app);
+LikesRoutes(app);
+UserRoutes(app);
 ModuleRoutes(app);
-AssignmentRoutes(app);
+CourseRoutes(app);
 Lab5(app);
-Hello(app);
-app.listen(process.env.PORT ||8000);
+HelloRoutes(app);
+SectionRoutes(app);
+EnrollmentRoutes(app);
+
+app.listen(4000);
