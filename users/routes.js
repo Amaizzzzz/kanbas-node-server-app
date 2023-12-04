2699ffc67a39dd415d0a5e774e69ffdcab3fd483
Amaizzzzz <112814055+Amaizzzzz@users.noreply.github.com>
2023-12-04 13:45:18 -0800

    modify accout

--- users/routes.js
+++ users/routes.js
@@ -66,7 +66,7 @@ function UserRoutes(app) {
   app.post("/api/users/signup", signup);
   app.post("/api/users/signin", signin);
   app.post("/api/users/signout", signout);
-  app.post("/api/users/account", account);
+  app.get("/api/users/account", account);
   
 }
 export default UserRoutes;
