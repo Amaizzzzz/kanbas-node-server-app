90e8f6c2977bfaed119d7186fc88c7c05ab8579e
Amaizzzzz <112814055+Amaizzzzz@users.noreply.github.com>
2023-12-04 12:42:13 -0800

    fix cors

--- app.js
+++ app.js
@@ -5,21 +5,12 @@ import mongoose from 'mongoose';
 import "dotenv/config";
 
 // Import routes
-import Hello from "./hello.js"
+import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import UserRoutes from "./users/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from './assignments/routes.js';
-
-// Ensure required environment variables are set
-const requiredEnv = ['FRONTEND_URL', 'MONGODB_URI'];
-requiredEnv.forEach(envVar => {
-  if (!process.env[envVar]) {
-    console.error(`Missing required environment variable: ${envVar}`);
-    process.exit(1);
-  }
-});
 
 // Setup MongoDB connection
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -30,11 +21,12 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 
 const app = express();
 
-const allowedOrigins = ['https://a6--creative-nougat-9b83f1.netlify.app', 'http://localhost:3000'];
+// CORS configuration
+const allowedOrigins = process.env.FRONTEND_URL.split(',');
 app.use(cors({
   credentials: true,
   origin: (origin, callback) => {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+    if (!origin || allowedOrigins.includes(origin.trim())) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
@@ -44,19 +36,19 @@ app.use(cors({
 
 // Session configuration
 const sessionOptions = {
-  secret: 'keyboard cat',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
-  cookie: {}
+  cookie: { secure: process.env.NODE_ENV === 'production' }
 };
 
+app.use(session(sessionOptions));
+
+// Trust first proxy if in production
 if (process.env.NODE_ENV === 'production') {
-  app.set('trust proxy', 1); // Trust first proxy
-  sessionOptions.cookie.secure = true; // Serve secure cookies
+  app.set('trust proxy', 1);
   sessionOptions.cookie.sameSite = 'none';
 }
-
-app.use(session(sessionOptions));
 
 // Middleware for JSON payload parsing
 app.use(express.json());
@@ -70,7 +62,7 @@ Lab5(app);
 Hello(app);
 
 // Start server
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
